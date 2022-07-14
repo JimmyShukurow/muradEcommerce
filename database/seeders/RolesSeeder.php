941a17b769e9910d7595d3a8c40f48a85b63642f
JimmyShukurow <jimishukurow@gmail.com>
2022-07-14 22:32:54 +0300

    new roles

--- database/seeders/RolesSeeder.php
+++ database/seeders/RolesSeeder.php
@@ -14,6 +14,12 @@ class RolesSeeder extends Seeder
         ],
         [
             'name' => 'admin',
+        ],
+        [
+            'name' => 'operator',
+        ],
+        [
+            'name' => 'wallet',
         ]
     ];
 
