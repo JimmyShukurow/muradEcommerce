37cff9258e43950c731bc8d13cb6466e31c2f94f
JimmyShukurow <jimishukurow@gmail.com>
2022-07-19 19:26:39 +0300

    ok

--- app/Http/Middleware/Wallet.php
+++ app/Http/Middleware/Wallet.php
@@ -4,13 +4,14 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class Wallet
 {
 
     public function handle(Request $request, Closure $next)
     {
-        $user = $request->user();
+        $user = Auth::user();
         if ($user->hasRole('wallet')) {
             return $next($request);
         }
